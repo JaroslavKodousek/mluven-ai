9aab18fd91570885248973790fdc44c75948072e
Jaroslav Kodousek <kodousek.jaroslav@gmail.com>
2025-07-21 13:04:05 +0200

    feat: Implement real-time audio streaming and language tutoring features in Streamlit app

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
-from src.mluvenai.utils.calling_openai import call_openai
 
 st.header("Welcome to the Mluven AI App")
-st.write("This is a simple Streamlit application to demonstrate the Mluven AI capabilities.")
+st.write(
+    "This is a simple Streamlit application to demonstrate the Mluven AI capabilities."
+)
 api_key_inserted = False
 
 with st.sidebar:
@@ -23,14 +24,24 @@ with st.sidebar:
 if api_key_inserted:
     st.write("You can now use the Mluven AI features.")
 
-    cefr_level = st.radio("Select your CEFR level:", 
-             options=["A1 (Absolute Beginner)", "A2 (Beginner)", "B1 (Intermediate)", "B2 (Upper Intermediate)", "C1 (Advanced)", "C2 (Proficient)"], 
-             index=2, 
-             help="Select your language proficiency level.")
+    cefr_level = st.radio(
+        "Select your CEFR level:",
+        options=[
+            "A1 (Absolute Beginner)",
+            "A2 (Beginner)",
+            "B1 (Intermediate)",
+            "B2 (Upper Intermediate)",
+            "C1 (Advanced)",
+            "C2 (Proficient)",
+        ],
+        index=2,
+        help="Select your language proficiency level.",
+    )
 
-    language_to_practice = st.selectbox("Select a language to practice:", 
-             options=["English", "German", "Spanish", "Polish", "Czech"],
-             index=0)
+    language_to_practice = st.selectbox(
+        "Select a language to practice:",
+        options=["English", "German", "Spanish", "Polish", "Czech"],
+        index=0,
+    )
     if st.button("Start Practicing"):
         st.write(f"Starting practice for {cefr_level} level in {language_to_practice}.")
-        call_openai(open_ai_apikey)
